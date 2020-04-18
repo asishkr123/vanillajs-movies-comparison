ba265ce6fb9ce1439e4f4960ad7b40dcc17e88af
asishkr <ashish.kumar@vidyalai.com>
2020-04-18 05:31:37 +0530

    done

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ createAutoComplete({
     onMovieSelect(movie, document.querySelector("#right-summary"), "right");
   }
 });
+
 let leftMovie;
 let rightMovie;
-
 const onMovieSelect = async (movie, summaryElement, side) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
@@ -53,8 +53,11 @@ const onMovieSelect = async (movie, summaryElement, side) => {
       i: movie.imdbID
     }
   });
+
+  summaryElement.innerHTML = movieTemplate(response.data);
+
   if (side === "left") {
-    leftMovie = respnse.data;
+    leftMovie = response.data;
   } else {
     rightMovie = response.data;
   }
@@ -62,13 +65,49 @@ const onMovieSelect = async (movie, summaryElement, side) => {
   if (leftMovie && rightMovie) {
     runComparison();
   }
+};
 
-  const runComparison = () => {};
+const runComparison = () => {
+  const leftSideStats = document.querySelectorAll(
+    "#left-summary .notification"
+  );
+  const rightSideStats = document.querySelectorAll(
+    "#right-summary .notification"
+  );
 
-  summaryElement.innerHTML = movieTemplate(response.data);
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+
+    const leftSideValue = leftStat.dataset.value;
+    const rightSideValue = rightStat.dataset.value;
+
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove("is-primary");
+      leftStat.classList.add("is-warning");
+    } else {
+      rightStat.classList.remove("is-primary");
+      rightStat.classList.add("is-warning");
+    }
+  });
 };
 
 const movieTemplate = movieDetail => {
+  const dollars = parseInt(
+    movieDetail.BoxOffice.replace(/\$/g, "").replace(/,/g, "")
+  );
+  const metascore = parseInt(movieDetail.Metascore);
+  const imdbRating = parseFloat(movieDetail.imdbRating);
+  const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ""));
+  const awards = movieDetail.Awards.split(" ").reduce((prev, word) => {
+    const value = parseInt(word);
+
+    if (isNaN(value)) {
+      return prev;
+    } else {
+      return prev + value;
+    }
+  }, 0);
+
   return `
     <article class="media">
       <figure class="media-left">
@@ -84,23 +123,24 @@ const movieTemplate = movieDetail => {
         </div>
       </div>
     </article>
-    <article class="notification is-primary">
+
+    <article data-value=${awards} class="notification is-primary">
       <p class="title">${movieDetail.Awards}</p>
       <p class="subtitle">Awards</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${dollars} class="notification is-primary">
       <p class="title">${movieDetail.BoxOffice}</p>
       <p class="subtitle">Box Office</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${metascore} class="notification is-primary">
       <p class="title">${movieDetail.Metascore}</p>
       <p class="subtitle">Metascore</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${imdbRating} class="notification is-primary">
       <p class="title">${movieDetail.imdbRating}</p>
       <p class="subtitle">IMDB Rating</p>
     </article>
-    <article class="notification is-primary">
+    <article data-value=${imdbVotes} class="notification is-primary">
       <p class="title">${movieDetail.imdbVotes}</p>
       <p class="subtitle">IMDB Votes</p>
     </article>
